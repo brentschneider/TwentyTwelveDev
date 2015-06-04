f6ae2eb6e8352655202da92f12eb29c23e7f9c55
Brent Schneider <brent@milkshakeinteractive.com>
2015-06-03 21:38:17 -0700

    basic api call to dribbble with callback_url answer

---
+++ angularjs grade notes/README.md
@@ -0,0 +1,16 @@
+
+
+
+
+
+To view in browser, open up a terminal and type:
+
+```
+$ cd /home/somedir
+$ python -m SimpleHTTPServer
+```
+That's it! Now your http server will start in port 8000. 
+
+You will get the message:
+
+```Serving HTTP on 0.0.0.0 port 8000 ..```
--- angularjs grade notes/js/controllers.js
+++ angularjs grade notes/js/controllers.js
@@ -1,1 +1,24 @@
 'use strict';
+
+
+// function AppCtrl ($scope) {
+// 	$scope.name = "to me";
+// }
+
+var controllers = angular.module('myAppCtrlr.controllers', []);
+
+controllers.controller('AppCtrl', function($scope){
+
+	$scope.name = "to me";
+
+})
+
+controllers.controller('ShotsListCtrl', function($scope, $http){
+	
+	$scope.list;
+	
+	$http.jsonp('http://api.dribbble.com/shots/popular?callback=JSON_CALLBACK').then(function(data){
+		console.log(data);
+	})
+
+})
