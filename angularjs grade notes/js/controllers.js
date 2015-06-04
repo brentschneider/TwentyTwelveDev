22953e86bc1c4f3f89b20e39c9d473a4ddbddc00
Brent Schneider <brent@milkshakeinteractive.com>
2015-06-03 23:49:08 -0700

    pulling in viewed / likes / comments too

--- angularjs grade notes/js/controllers.js
+++ angularjs grade notes/js/controllers.js
@@ -9,7 +9,8 @@ var controllers = angular.module('myAppCtrlr.controllers', []);
 
 controllers.controller('AppCtrl', function($scope){
 
-	$scope.name = "to me";
+	$scope.title = "Angular & Dribbble";
+	$scope.description = "Using Angular to pull popular shots via the API from dribbble!";
 
 })
 
@@ -18,6 +19,7 @@ controllers.controller('ShotsListCtrl', function($scope, $http){
 	$scope.list;
 	
 	$http.jsonp('http://api.dribbble.com/shots/popular?callback=JSON_CALLBACK').then(function(data){
+		$scope.list = data.data;
 		console.log(data);
 	})
 
